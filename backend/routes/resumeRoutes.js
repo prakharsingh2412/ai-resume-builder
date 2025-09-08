d87118154454e5aeaea452b539b45ea138e92c73
Prakhar Singh <prakharsingh98765432@gmail.com>
2025-09-08 13:07:25 +0530

    bug fix

--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { auth } from "../middleware/auth.js";
 import { generateResume, getResumes , getResumeById, updateResume, deleteResume ,uploadResume, scoreResume } from "../controllers/resumeController.js";
-import upload from "../middleware/upload.js";
+import upload from "../uploads/upload.js";
 
 const router = express.Router();
 router.use(auth);
